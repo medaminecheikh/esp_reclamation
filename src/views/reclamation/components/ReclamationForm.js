38433837aea2ca0645d5790da6b17dbb93f8b487
medaminecheikh <amine_cheikhh@outlook.fr>
2024-04-01 18:22:53 +0100

    first

---
+++ src/views/reclamation/components/ReclamationForm.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { Avatar, Box, Button, Checkbox, FormControlLabel, Grid, Link, TextField, Typography } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import SendIcon from '@mui/icons-material/Send';
+
+function ReclamationForm() {
+  return (
+    <Box
+      sx={{
+        marginTop: 10,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: '#EEEEEE', boxShadow:'6' }}>
+        <LockOutlinedIcon color={'secondary'}/>
+      </Avatar>
+      <Typography component="h1" variant="h5" sx={{ mt: 1 }}>
+        Remplir Reclamation
+      </Typography>
+      <Box component="form" noValidate onSubmit={null} sx={{ mt: 3 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              autoComplete="given-name"
+              name="firstName"
+              required
+              fullWidth
+              id="firstName"
+              label="First Name"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              fullWidth
+              id="lastName"
+              label="Last Name"
+              name="lastName"
+              autoComplete="family-name"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="new-password"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={<Checkbox value="allowExtraEmails" color="primary" />}
+              label="I want to receive inspiration, marketing promotions and updates via email."
+            />
+          </Grid>
+        </Grid>
+        <Button
+          endIcon={<SendIcon />}
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2, bgcolor:'error.dark' }}
+        >
+          Envoyer Réclamation
+        </Button>
+        <Grid container justifyContent="flex-end">
+          <Grid item>
+            <Link href="#" variant="body2">
+              Already have an account? Sign in
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+}
+
+export default ReclamationForm;
--- src/views/reclamation/reclamation.js
+++ src/views/reclamation/reclamation.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+import AppAppBar from './components/topbar';
+import { Box, CssBaseline, Grid, Paper } from '@mui/material';
+import ReclamationForm from './components/ReclamationForm';
+import { styled } from '@mui/material/styles';
 
-class Reclamation extends Component {
-  render() {
-    return (
-      <div>
-        <h1>RECLAMATION</h1>
-      </div>
-    );
-  }
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+export default function Reclamation() {
+  const [mode, setMode] = React.useState('light');
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+  return (
+    <div>
+      <CssBaseline />
+
+      <AppAppBar mode={mode} toggleColorMode={toggleColorMode}></AppAppBar>
+      <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Item></Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item> <ReclamationForm /></Item>
+        </Grid>
+        <Grid item xs>
+          <Item></Item>
+        </Grid>
+      </Grid>
+      </Box>
+    </div>
+  );
 }
-
-export default Reclamation;
