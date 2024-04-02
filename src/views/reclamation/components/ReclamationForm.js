6436b57878903075bb8522f31149452ae2cebe07
medaminecheikh <amine_cheikhh@outlook.fr>
2024-04-02 01:08:19 +0100

    reclamation

--- src/views/reclamation/components/ReclamationForm.js
+++ src/views/reclamation/components/ReclamationForm.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import { Avatar, Box, Button, Checkbox, FormControlLabel, Grid, Link, TextField, Typography } from '@mui/material';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import React, { useState } from 'react';
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  Grid,
+  IconButton,
+  InputLabel,
+  Link,
+  MenuItem,
+  Select,
+  TextField,
+  Typography
+} from '@mui/material';
+import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
 import SendIcon from '@mui/icons-material/Send';
+import CloseIcon from '@mui/icons-material/Close';
 
 function ReclamationForm() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleRemoveFile = () => {
+    setSelectedFile(null);
+  };
+
   return (
     <Box
       sx={{
-        marginTop: 10,
+        marginTop: 5,
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center'
       }}
     >
-      <Avatar sx={{ m: 1, bgcolor: '#EEEEEE', boxShadow:'6' }}>
-        <LockOutlinedIcon color={'secondary'}/>
+      <Avatar sx={{ m: 1, bgcolor: '#EEEEEE', boxShadow: '6' }}>
+        <HistoryEduIcon  sx={{ color: '#212121' }} />
       </Avatar>
-      <Typography component="h1" variant="h5" sx={{ mt: 1 }}>
+      <Typography component="h1" variant="h4" sx={{ mt: 1 }}>
         Remplir Reclamation
       </Typography>
-      <Box component="form" noValidate onSubmit={null} sx={{ mt: 3 }}>
-        <Grid container spacing={2}>
+      <Box component="form" noValidate onSubmit={null} sx={{ mt: 3}}>
+        <Grid container spacing={2} justifyContent={'space-evenly'}>
           <Grid item xs={12} sm={6}>
             <TextField
               autoComplete="given-name"
@@ -42,40 +69,99 @@ function ReclamationForm() {
               autoComplete="family-name"
             />
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-            />
+
+          <Grid item xs={12} sm={4}>
+            <FormControl fullWidth>
+              <InputLabel id="application-select-label">Application</InputLabel>
+              <Select
+                fullWidth
+                required
+                labelId="application-select-label"
+                id="application"
+                name="application"
+
+              >
+                <MenuItem value="">Choisir une application</MenuItem>
+                {/* Add options for applications here */}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <FormControl fullWidth>
+              <InputLabel id="problem-select-label">Problème</InputLabel>
+              <Select
+                fullWidth
+                labelId="problem-select-label"
+                id="problem"
+                name="problem"
+
+              >
+                <MenuItem value="">Choisir un problème</MenuItem>
+                {/* Add options for problems here */}
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <FormControl fullWidth>
+              <InputLabel id="problem-select-label">Priorité</InputLabel>
+              <Select
+                fullWidth
+                labelId="problem-select-label"
+                id="Priorite"
+                name="Priorité"
+
+              >
+                <MenuItem value="">Choisir un problème</MenuItem>
+                {/* Add options for problems here */}
+              </Select>
+            </FormControl>
           </Grid>
           <Grid item xs={12}>
             <TextField
               required
               fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="new-password"
+              label="Description Problème"
+              name="descriptionProbleme"
+              id="descriptionProbleme"
+              multiline
+              rows={5} // Adjust the number of rows as needed
+              placeholder="Veuillez décrire le problème en détail"
             />
           </Grid>
-          <Grid item xs={12}>
+          <Grid item xs={12} display="flex" justifyContent="flex-start">
+            <TextField
+              id="file-input"
+              type="file"
+              label="Sélectionner un fichier" // Label
+              onChange={handleChange}
+              focused
+              fullWidth // To occupy full width
+            />
+            {selectedFile && (
+              <div style={{ display: "flex", alignItems: "center", marginLeft: 10 }}>
+                <Typography variant="body2" color="text.secondary">
+                  {selectedFile.name}
+                </Typography>
+                <IconButton onClick={handleRemoveFile} sx={{ marginLeft: 5 }}>
+                  <CloseIcon fontSize="small" />
+                </IconButton>
+              </div>
+            )}
+          </Grid>
+          <Grid item xs={12} display="flex" justifyContent="end">
             <FormControlLabel
               control={<Checkbox value="allowExtraEmails" color="primary" />}
-              label="I want to receive inspiration, marketing promotions and updates via email."
+              label="I want to receive updates via email."
             />
           </Grid>
         </Grid>
         <Button
           endIcon={<SendIcon />}
           type="submit"
+          color={'info'}
           fullWidth
           variant="contained"
-          sx={{ mt: 3, mb: 2, bgcolor:'error.dark' }}
+          sx={{ mt: 3, mb: 2, bgcolor: 'error.dark' }}
         >
           Envoyer Réclamation
         </Button>
--- src/views/reclamation/reclamation.js
+++ src/views/reclamation/reclamation.js
@@ -7,7 +7,7 @@ import { styled } from '@mui/material/styles';
 
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#F5F5F5',
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: 'center',
@@ -19,23 +19,22 @@ export default function Reclamation() {
     setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
   };
   return (
-    <div>
-      <CssBaseline />
-
-      <AppAppBar mode={mode} toggleColorMode={toggleColorMode}></AppAppBar>
-      <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={3}>
-        <Grid item xs>
+    <Box sx={{display: 'flex', flexGrow: 1, bgcolor: '#F5F5F5', width: '100%', height: '100vh', alignItems: 'center'}} >
+    <CssBaseline />
+    <AppAppBar mode={mode} toggleColorMode={toggleColorMode}></AppAppBar>
+    <Box sx={{ flexGrow: 1, bgcolor: '#F5F5F5' }} >
+      <Grid container spacing={3} sx={{ bgcolor: '#F5F5F5' }}>
+        <Grid item xs sx={{alignItems:'center', bgcolor: '#F5F5F5'}}>
           <Item></Item>
         </Grid>
-        <Grid item xs={8}>
+        <Grid item xs={8} sm={7} sx={{alignItems:'center', bgcolor: '#F5F5F5'}}>
           <Item> <ReclamationForm /></Item>
         </Grid>
-        <Grid item xs>
+        <Grid item xs sx={{alignItems:'center', bgcolor: '#F5F5F5'}}>
           <Item></Item>
         </Grid>
       </Grid>
-      </Box>
-    </div>
+    </Box>
+  </Box>
   );
 }
