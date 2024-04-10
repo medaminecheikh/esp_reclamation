44179fdf444a87b97807b4db26c16ea3fe0ebabd
medaminecheikh <amine_cheikhh@outlook.fr>
2024-04-10 04:32:24 +0100

    reclamation

--- src/layout/MainLayout/LogoSection/index.js
+++ src/layout/MainLayout/LogoSection/index.js
@@ -1,24 +1,35 @@
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import {Link} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
 
 // material-ui
-import { ButtonBase } from '@mui/material';
+import {ButtonBase} from '@mui/material';
 
 // project imports
 import config from 'config';
-import Logo from 'ui-component/Logo';
-import { MENU_OPEN } from 'store/actions';
-
+import {MENU_OPEN} from 'store/actions';
+import logo from '../../../assets/images/logo.svg';
+import * as React from "react";
 // ==============================|| MAIN LOGO ||============================== //
-
+const logoStyle = {
+    width: '140px',
+    height: 'auto',
+    cursor: 'pointer'
+};
 const LogoSection = () => {
-  const defaultId = useSelector((state) => state.customization.defaultId);
-  const dispatch = useDispatch();
-  return (
-    <ButtonBase disableRipple onClick={() => dispatch({ type: MENU_OPEN, id: defaultId })} component={Link} to={config.defaultPath}>
-      <Logo />
-    </ButtonBase>
-  );
+    const defaultId = useSelector((state) => state.customization.defaultId);
+    const dispatch = useDispatch();
+    return (
+        <ButtonBase disableRipple onClick={() => dispatch({type: MENU_OPEN, id: defaultId})} component={Link}
+                    to={config.defaultPath}>
+            <img
+                src={
+                    logo
+                }
+                style={logoStyle}
+                alt="ESPRIT"
+            />
+        </ButtonBase>
+    );
 };
 
 export default LogoSection;
