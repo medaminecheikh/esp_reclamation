25f217ee82d8b744b1438d7dbf0daf42ed5e1866
medaminecheikh <amine_cheikhh@outlook.fr>
2024-04-20 16:35:48 +0100

    reclamation

--- src/views/historiqueReq/historiqueComp/IssueProject.js
+++ src/views/historiqueReq/historiqueComp/IssueProject.js
@@ -5,14 +5,15 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 import ImageIcon from '@mui/icons-material/Image';
-import WorkIcon from '@mui/icons-material/Work';
-import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
+
+
 function IssueProject({myProjects}) {
      const [projects, setProjects] = useState(null);
+    
       // useEffect to update state whenever myProjects changes
     useEffect(() => {
         if (myProjects) {
@@ -23,13 +24,35 @@ function IssueProject({myProjects}) {
      useEffect(() => {
         if (projects) {
             console.log('Updated projects:', projects);
+            const arrayProject = projects.issues.map(issue => ({
+                id: issue.fields.project.id,
+                key: issue.fields.project.key,
+                name: issue.fields.project.name,
+                // Add more properties as needed
+            }));
+            console.log('Array of projects:', arrayProject);
+            const uniqueProjects = [];
+            arrayProject.forEach(project => {
+            // Check if the project ID already exists in uniqueProjects
+            const exists = uniqueProjects.some(item => item.id === project.id);
+            
+            // If the project ID doesn't exist, add it to uniqueProjects
+            if (!exists) {
+                uniqueProjects.push(project);
+            }
+        });
+        
+        console.log('Array of uniqueProjects:', uniqueProjects);
         }
     }, [projects]);
+
+
   return (
     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            <Divider variant="inset" component="li" />
-           
-      <ListItem>
+    {projects?.issues.map((issue, index)=>(
+        <React.Fragment key={index}>
+<Divider variant="inset" component="li" />
+    <ListItem>
          <ListItemAvatar>
             <Avatar>
              <ImageIcon />
@@ -38,7 +61,7 @@ function IssueProject({myProjects}) {
                   <Grid container justifyContent="space-between" alignItems="center">
                 <Grid item>
                  
-                    <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+                    <ListItemText primary={issue && issue.project && issue.project.name ? issue.project.name : "Unknown"} secondary="Jan 9, 2014" />
                 </Grid>
                 <Grid item>
                    <Avatar variant="rounded" sx={{  bgcolor: 'error.main',
@@ -49,28 +72,10 @@ function IssueProject({myProjects}) {
                     </Avatar>
                 </Grid>
             </Grid>
-        </ListItem>
-            <Divider variant="inset" component="li" />
-
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <WorkIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Work" secondary="Jan 7, 2014" />
-      </ListItem>
-            <Divider variant="inset" component="li" />
-
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <BeachAccessIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Vacation" secondary="July 20, 2014" />
-      </ListItem>
-      
+    </ListItem>
+    </React.Fragment>
+    ))}
+         
     </List>
   )
 }
