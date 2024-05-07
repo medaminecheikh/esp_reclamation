7dfa95d496d8c7cd10c3e3732c7b69e50cab443d
medaminecheikh <amine_cheikhh@outlook.fr>
2024-05-07 12:25:05 +0100

    first

--- src/views/application.js
+++ src/views/application.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import MainCard from '../ui-component/cards/MainCard';
-import TopBar from './appCompo/TopBar';
-import { Grid } from '@mui/material';
+
+import { Button, FormControl, Grid, MenuItem, Select, Typography } from '@mui/material';
 import DetailIssue from './appCompo/DetailIssue';
 import ListIssuesAdmin from './appCompo/ListIssues';
+import AnimateButton from 'ui-component/extended/AnimateButton';
 
 
 
@@ -14,17 +15,159 @@ function Application() {
 
 
     const [selectedUser, setSelectedUser] = useState(null);
+    const [site, setSite] = useState('');
+    const [status, setStatus] = useState('');
+    const [priorite, setPriorite] = useState('');
 
     const handleUserSelect = (user) => {
       setSelectedUser(user);
       
     };
 
-   
+    const handleChangeSite = (event) => {
+      setSite(event.target.value);
+    };
+    const handleChangestatus = (event) => {
+      setStatus(event.target.value);
+    };
+    const handleChangePriorite = (event) => {
+      setPriorite(event.target.value);
+    };
+   const handleCancel=()=>{
+    setPriorite('');
+    setStatus('');
+    setSite('');
+   }
+   const handleSearch = ()=>{}
     
     return (<>
       <MainCard >
-          <TopBar />
+      <Grid container spacing={3}>
+      <Grid item xs={12} sm={6} lg={3}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography variant="subtitle2">Site Web </Typography>
+            </Grid>
+            <Grid item xs={12}>
+   <FormControl sx={{ minWidth: 150 }} size="small">
+    
+      <Select
+        id="demo-select-small"
+        value={site}
+        onChange={handleChangeSite}
+      >
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+        <MenuItem value={10}>Ten</MenuItem>
+        <MenuItem value={20}>Twenty</MenuItem>
+        <MenuItem value={30}>Thirty</MenuItem>
+      </Select>
+    </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography variant="subtitle2">Status </Typography>
+            </Grid>
+            <Grid item xs={12}>
+  <FormControl sx={{ minWidth: 150 }} size="small">
+    
+    <Select
+      id="status"
+      value={status}
+      onChange={handleChangestatus}
+    >
+      <MenuItem value="">
+        <em>None</em>
+      </MenuItem>
+      <MenuItem value={10}>Ten</MenuItem>
+      <MenuItem value={20}>Twenty</MenuItem>
+      <MenuItem value={30}>Thirty</MenuItem>
+    </Select>
+  </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={12} sm={6} lg={3}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography variant="subtitle2">Priorite </Typography>
+            </Grid>
+            <Grid item xs={12}>
+            <FormControl sx={{ minWidth: 150 }} size="small">
+    
+    <Select
+      id="prio"
+      value={priorite}
+      onChange={handleChangePriorite}
+    >
+      <MenuItem value="">
+        <em>None</em>
+      </MenuItem>
+      <MenuItem value={10}>Ten</MenuItem>
+      <MenuItem value={20}>Twenty</MenuItem>
+      <MenuItem value={30}>Thirty</MenuItem>
+    </Select>
+  </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Typography variant="subtitle2">Options </Typography>
+            </Grid>
+            <Grid item xs={8}>   
+              <AnimateButton>
+                <Button
+                  disableElevation
+                  fullWidth
+                  size="small"
+                  type="button"
+                  variant="contained"
+                  color="warning"
+                  onClick={handleSearch}
+                >
+                  Search
+                </Button>
+              </AnimateButton>
+              </Grid>
+            <Grid item xs={4}>
+        
+              <AnimateButton>
+                <Button
+                  disableElevation
+                  fullWidth
+                  size="small"
+                  type="button"
+                  variant="contained"
+                  color="inherit"
+                  onClick={handleCancel}
+                >
+                  Reset
+                </Button>
+              </AnimateButton>
+            </Grid>
+        
+            <Grid item xs={12}>
+              
+            </Grid>
+          </Grid>
+        </Grid>
+   </Grid>
       </MainCard>
       <Grid container spacing={1} style={{marginTop:'3px'}}>
         <Grid item xs={7}>
--- src/views/projet.js
+++ src/views/projet.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import MainCard from '../ui-component/cards/MainCard';
-import { Typography } from '@mui/material';
+import { Grid } from '@mui/material';
+import TopBar from './appCompo/TopBar';
 
 class Projet extends Component {
   render() {
-    return (
-      <MainCard title="Projet Card">
-        <Typography variant="body2">
-          Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-          minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-          in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-          descent molls anim id est labours.
-        </Typography>
+    return (<>
+      <MainCard >
+          <TopBar />
       </MainCard>
+      <Grid container spacing={1} style={{marginTop:'3px'}}>
+        <Grid item xs={7}>
+        <MainCard >
+     
+        </MainCard>
+        </Grid>
+        <Grid item xs={5}>
+        <MainCard >
+         
+        </MainCard>
+        </Grid>
+      </Grid>
+      </>
     );
   }
 }
